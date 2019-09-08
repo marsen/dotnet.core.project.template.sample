5e35c70ba7c2b51c6f8612699fdc025c28c881d2
marklin <marklin@nine-yi.com>
2019-09-08 17:44:36 +0800

    add template

---
+++ Blog.Plugins.F-plugin-name/F-plugin-namePlugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Marsen.Blog.Models.Requests.Payments;
+using Marsen.Blog.Models.Response.Payments;
+using Marsen.Blog.Plugins.Interfaces;
+
+namespace Marsen.Blog.Plugins.(C-plugin-name)
+{
+    public class (C-plugin-name)Plugin : IMiddlewarePlugin
+    {
+        public Task<PayEntity> Pay(Request request)
+        {
+            var result = new Task<PayEntity>(() => new PayEntity
+            {
+                ReturnMessage = "(C-plugin-name) Pay"
+            });
+            
+            result.RunSynchronously();
+            return result;
+        }
+
+        public Task<RefundEntity> Refund(Request request)
+        {
+            var result = new Task<RefundEntity>(()=>new RefundEntity
+            {
+                ReturnMessage = "(C-plugin-name) Return"
+            });
+
+            result.RunSynchronously();
+            return result;
+        }
+    }
+}
